8fa1e411596dfc37d015361d6992d29ef1f1d529
Ituxcoza <peter@itux.co.za>
2012-08-05 16:09:43 +0200

    Added AVR-Burher start script for Terminal-less running

---
+++ AVR-Burner/Software/run-avr-burner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# OpenHardware.co.za - and Cmdrkeen.net - Firmware Burning Script for AVRs
+# (C) 2012
+
+
+./functional.sh 2>&1 | zenity --text-info --width 500 --height 400 --title "Done:  Output Log"
